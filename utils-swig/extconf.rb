f7a517f9794957dcbc219b631ae011821d776ccc
alex <alex@be07c712-5235-0410-95d4-c99efef06bb2>
2008-09-05 17:50:26 +0000

    adding a versioned symlink... lets see if this works
    git-svn-id: svn+ssh://yew/home/alex/code-repo/projects/datajockey/code/qtgui@723 be07c712-5235-0410-95d4-c99efef06bb2

--- utils-swig/extconf.rb
+++ utils-swig/extconf.rb
@@ -1,7 +1,9 @@
 require 'mkmf'
 `swig -w801 -c++ -ruby utilities.i`
+
 $libs = append_library($libs, "stdc++")
 $libs = append_library($libs, "sndfile")
 $libs = append_library($libs, "mad")
+$libs = append_library($libs, "vorbisfile")
 dir_config(".", ["../include/"], "..")
 create_makefile('datajockey_utils')
