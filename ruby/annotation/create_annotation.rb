1ae05f4bc5e81c91c9790076567be7e366ef58ee
alex <alex@be07c712-5235-0410-95d4-c99efef06bb2>
2008-09-05 20:27:02 +0000

    grabbing id3 tag data as well now, putting it into yaml file
    git-svn-id: svn+ssh://yew/home/alex/code-repo/projects/datajockey/code/qtgui@731 be07c712-5235-0410-95d4-c99efef06bb2

--- ruby/annotation/create_annotation.rb
+++ ruby/annotation/create_annotation.rb
@@ -4,11 +4,30 @@
 #how to run: ruby -I ruby/ ./ruby/annotation/create_annotation.rb /tmp/test.mp3
 #
 
+require 'rubygems'
+require 'rtaglib'
 require 'yaml'
 require 'annotation/beatlocations'
 
+class TagFile::File
+  def has_attribute(name)
+    if !self.send(name) or self.send(name) =~ /^\s*$/
+      return false
+    else
+      return true
+    end
+  end
+end
+
 module Datajockey
   module Annotation
+    def Annotation::set_tag_data(tag_data,attribute,tag)
+      if !tag.send(attribute) or tag.send(attribute) =~ /^\s*$/
+        return false
+      else
+        tag_data[attribute] = tag.send(attribute)
+      end
+    end
     def Annotation::createAnnotation(audioFile)
       annotation = Hash.new
       smoothing = 1
@@ -33,6 +52,20 @@ module Datajockey
       cur["mtime"] = cur["ctime"] = DateTime.now
       cur["source"] = "Datajockey::Annotation::smoothNumArray(Datajockey::Annotation::getBeatLocations(#{audioFile}), #{smoothing})"
       annotation["beat locations"] << cur
+      annotation["tags"] = Hash.new
+      begin
+        tag = TagFile::File.new(audioFile)
+        set_tag_data(annotation,"title",tag)
+        set_tag_data(annotation,"artist",tag)
+        set_tag_data(annotation["tags"],"genre",tag)
+        set_tag_data(annotation,"year",tag)
+        if tag.has_attribute("album")
+          annotation["album"] = album = Hash.new
+          album["name"] = tag.album
+          set_tag_data(album,"track",tag)
+        end
+      rescue TagFile::BadFile
+      end
 
       return annotation
     end
