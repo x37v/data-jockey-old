9c2bc2ab1f6c75a655ab00493e0f71e55ac5fb1f
alex <alex@be07c712-5235-0410-95d4-c99efef06bb2>
2008-09-05 20:05:16 +0000

    we can now create the annotation files
    git-svn-id: svn+ssh://yew/home/alex/code-repo/projects/datajockey/code/qtgui@729 be07c712-5235-0410-95d4-c99efef06bb2

---
+++ ruby/annotation/create_annotation.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+require 'yaml'
+#this is in the same directory..
+require File.join(File.expand_path(File.dirname(__FILE__)), 'beatlocations')
+
+module Datajockey
+  module Annotation
+    def Annotation::createAnnotation(audioFile)
+      annotation = Hash.new
+      smoothing = 1
+      conf_file = File.join(ENV["HOME"], ".datajockey", "config.yaml")
+      if File.exists?(conf_file)
+        config = YAML::load(File.open(conf_file))
+        if(config["annotation"]["beat locations"]["smoothing"])
+          smoothing = config["annotation"]["beat locations"]["smoothing"]
+        end
+      end
+
+      annotation["beat locations"] = Array.new
+
+      cur = Hash.new
+      cur["timepoints"] = beats = getBeatLocations(audioFile)
+      cur["mtime"] = cur["ctime"] = DateTime.now
+      cur["source"] = "Datajockey::Annotation::getBeatLocations(#{audioFile})"
+      annotation["beat locations"] << cur
+
+      cur = Hash.new
+      cur["timepoints"] = smoothNumArray(beats, smoothing)
+      cur["mtime"] = cur["ctime"] = DateTime.now
+      cur["source"] = "Datajockey::Annotation::smoothNumArray(Datajockey::Annotation::getBeatLocations(#{audioFile}), #{smoothing})"
+      annotation["beat locations"] << cur
+
+      return annotation
+    end
+  end
+end
+
+if __FILE__ == $0
+  puts Datajockey::Annotation::createAnnotation(ARGV[0]).to_yaml
+end
+
+#puts Datajockey::Annotation::createAnnotation("/tmp/test.mp3").to_yaml
