5da8dc2c8d706a66d68e1a5403f890884d8ad707
alex <alex@be07c712-5235-0410-95d4-c99efef06bb2>
2008-12-01 04:06:06 +0000

    added GPL text, added to BUGS
    git-svn-id: svn+ssh://yew/home/alex/code-repo/projects/datajockey/code/qtgui@893 be07c712-5235-0410-95d4-c99efef06bb2

--- ruby/datajockey/annotation/create_annotation.rb
+++ ruby/datajockey/annotation/create_annotation.rb
@@ -4,6 +4,23 @@
 #how to run: ruby -Iruby/ ./ruby/annotation/create_annotation.rb /tmp/test.mp3
 #
 
+=begin
+	This file is part of Data Jockey.
+	
+	Data Jockey is free software: you can redistribute it and/or modify it
+	under the terms of the GNU General Public License as published by the
+	Free Software Foundation, either version 3 of the License, or (at your
+	option) any later version.
+	
+	Data Jockey is distributed in the hope that it will be useful, but
+	WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+	Public License for more details.
+	
+	You should have received a copy of the GNU General Public License along
+	with Data Jockey.  If not, see <http://www.gnu.org/licenses/>.
+=end
+
 require 'datajockey/base'
 require 'datajockey/annotation/beatlocations'
 require 'rubygems'
